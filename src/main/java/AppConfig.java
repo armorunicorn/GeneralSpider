74e08684a8777eb4196d4aa4762887e94b0a12f2
saber <armorunicorn@gmail.com>
2016-03-03 17:50:09 +0800

    0.0.1

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+/**
+ * Created by saber on 16/1/18.
+ * 应用程序配置类
+ */
+public class AppConfig
+{
+    public String mongo_url;
+    public String mongo_collection;
+    public List<SpiderConfig> spiderConfigList;
+}
